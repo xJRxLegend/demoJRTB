4b69010d38328e67e9cddb0f2bc57c4afd687e71
xJRxLegend <86181370+xJRxLegend@users.noreply.github.com>
2021-07-06 21:41:59 +0400

    Add files via upload

---
+++ src/main/java/com/github/javarushcommunity/javarushtelegrambotmy/JavarushTelegrambotmyApplication.java
@@ -0,0 +1,13 @@
+package com.github.javarushcommunity.javarushtelegrambotmy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavarushTelegrambotmyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavarushTelegrambotmyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/javarushcommunity/javarushtelegrambotmy/JavarushTelegrambotmyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.javarushcommunity.javarushtelegrambotmy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavarushTelegrambotmyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
